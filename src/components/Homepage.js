e2404da327f6eea9fed4dbfdef2556357522bc63
Christina Li <christinayxli@gmail.com>
2022-12-22 17:54:18 -0500

    created navbar, styled the title for the 'reflect' page.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,4 @@
 import './Homepage.css'
-import button from './button'
 import Reflect from './reflect'
 import { Routes, Route, useNavigate} from 'react-router-dom';
 import Create from './create'
@@ -18,7 +17,7 @@ function Homepage() {
       <>
       <div className = 'w-screen h-screen grid grid-rows-2 text-white md: grid-cols-2'>
         <div className = 'tracking-wide centered font-mono font-bold w-full h-ful firstbox bg-black md:h-screen text-6xl'>
-            <p> Reflect </p>
+            <p className = 'text-6xl'> Reflect </p>
             <div>
             <button onClick={navigateToReflect} className = 'text-2xl font-normal tracking-wider font-sans bg-[#525962] hover:bg-[#2F2E2D]'>  &nbsp; click to start &nbsp; </button>
             <Routes>
@@ -28,11 +27,11 @@ function Homepage() {
         </div>
 
         <div className = 'tracking-wide centered font-mono font-bold w-full h-ful secondbox bg-black md:h-screen text-6xl'>
-            <p> Create </p>
+            <p className = 'text-6xl'> Create </p>
             <div>
             <button onClick={navigateToCreate} className ='text-2xl font-normal tracking-wider font-sans bg-[#917967] hover:bg-[#827A72]'>  &nbsp; click to start &nbsp;</button>
             <Routes>
-              <Route path="./create" element={<Reflect/>} />
+              <Route path="./create" element={<Create/>} />
         </Routes>
             </div>
         </div>
--- src/components/reflect.js
+++ src/components/reflect.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './reflect.css'
+import Navbar from './navbar'
 
 function reflect() {
   return (
     <div>
-        <p> Hello  There</p>
+      <Navbar/>
+      <div className = 'centered'>
+      <h1 className ="text-center mb-4 text-2xl font-bold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-[#596789] from-[#80A3B8]"> Reflect | </span> | Meraki </h1>
+      </div>
     </div>
 
   )

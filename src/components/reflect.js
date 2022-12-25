8b111389ac9a002a98ffd1c3f5f00363210689e1
Christina Li <christinayxli@gmail.com>
2022-12-25 13:12:08 -0500

    updated prompts

--- src/components/reflect.js
+++ src/components/reflect.js
@@ -13,7 +13,7 @@ function reflect() {
       <div className = 'centered'>
       <h1 className =" font-serif tracking-wide text-center mb-4 text-4xl font-semibold text-gray-900 dark:text-white md:text-4xl lg:text-5xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-[#596789] from-[#80A3B8]"> Reflect | </span> | Meraki </h1>
       <p className = 'font-light font-sans text-xl text-center italic md:text-base lg:text-xl'> Your one place for <mark class="bg-[#f7e8e6]"> &nbsp; reflection &nbsp;  </mark>   &nbsp; – anytime, anywhere.</p>
-      <Alert className = 'bg-[#6D8891]' markstyle='bg-[#F7E8E6]' link = 'https://gist.githubusercontent.com/christinaly19/031afcd1e22150a6a3db72d9d9e6eb8c/raw/13c797a774b4b9f50cce2cd06696e3c1908e9f98/quotes.json'>
+      <Alert className = 'bg-[#6D8891]' markstyle='bg-[#F7E8E6]' link = 'https://gist.githubusercontent.com/christinaly19/031afcd1e22150a6a3db72d9d9e6eb8c/raw/c6c12a6f1f3b79ac62a37a43177b039abf140a77/quotes.json'>
       </Alert>    
      <div className = 'spacebefore1'></div>
       <Writingbox></Writingbox>

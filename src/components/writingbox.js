f29f3dd0f72d7ae44c3422ae7977e1c256387683
Christina Li <christinayxli@gmail.com>
2023-01-06 14:41:26 -0500

    added unsplash api

--- src/components/writingbox.js
+++ src/components/writingbox.js
@@ -1,10 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './writingbox.css'
 import Button from './button'
+
 function Writingbox() {
-  const [query, setQuery] = useState("");
+const [img, setImg] = useState("");
+const [res, setRes] = useState([]);
+
+const fetchRequest = async () => {
+  const data = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${'BPz3g4RcwRZH71JTmq-_3rAcWQiOZ_sBmV2zNYnd39g'}`
+  );
+  const dataJ = await data.json();
+  const result = dataJ.results;
+  console.log(result);
+  setRes(result);
+};
+
+useEffect(() => {
+  fetchRequest();
+}, []);
+
+
+const Submit = () => {
+  fetchRequest();
+  setImg("");
+};
+
   return (
     <div className = 'writingbox'>
+
         <label for="message" class="font-serif block mb-2 text-xl font-semibold  text-transparent bg-clip-text bg-gradient-to-r to-[#553e52] from-[#a57ca0]">Start Writing:</label>
         <p className = 'italic font-light'> Ready to start? Click 'start' in the timer below, and your timer for 10 minutes (600 seconds) will begin. </p>
         <div className = 'spacing'></div>
@@ -15,10 +39,12 @@ function Writingbox() {
           name="query"
           className="input"
           placeholder={`Start typing here!`}
+          value = {img}
+          onChange = {(e) => setImg(e.target.value)}
         />
        </div>
-      <Button className = 'mt-2 button ml-2 text-white bg-[#6D8891] hover:bg-[#445054]'>Submit </Button>
-     
+      <Button  onClick = {Submit} className = 'mt-2 button ml-2 text-white bg-[#6D8891] hover:bg-[#445054]'>Submit </Button>
+      
     </div>
   )
 }
